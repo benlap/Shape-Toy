e2fb8c1584328604042e95a42052d9118b25bf3d
Ben Lapid <benlap@umich.edu>
2021-01-10 14:56:55 -0500

    Add DoubleClickHandler

---
+++ Assets/Scripts/DoubleClickHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DoubleClickHandler : MonoBehaviour,IPointerDownHandler
+{
+    private float clickCount = 0;
+    private float clickTime = 0;
+    private float clickDelay = 0.5f;
+
+    private bool secondClickCameInTime { get { return clickCount > 1 && Time.time - clickTime < clickDelay; } }
+
+    [SerializeField]
+    private UnityEvent onDoubleClick;
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        clickCount++;
+
+        if (clickCount == 1)
+        {
+            clickTime = Time.time;
+        }
+
+        if (secondClickCameInTime)
+        {
+            clickCount = 0;
+            clickTime = 0;
+            Debug.Log("Double click on " + name);
+            onDoubleClick.Invoke();
+        }
+        else if (clickCount > 2 || Time.time - clickTime > 1)
+        {
+            clickCount = 0;
+        }
+    }   
+}
