92bb0061db1fa2e7eb68da3d0a9bdecc0a9f8a47
Ben Lapid <benlap@umich.edu>
2021-01-11 20:21:50 -0500

    Add Quit App button

---
+++ Assets/QuitApplication.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ShapeToy
+{
+    public class QuitApplication : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
